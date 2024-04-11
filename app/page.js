fbf1ea00ffb283889f239fdcacd51a20cf3d71f9
LoLoSenPai <loic.dlugosz@gmail.com>
2024-04-11 18:50:46 +0200

    Update social media links and button styling in Home component

--- app/page.js
+++ app/page.js
@@ -2,13 +2,13 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="relative" style={{ backgroundImage: 'url(/images/banner.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
+    <div className='pb-6 bg-red-900/10'>
 
       <div className="relative text-center text-white">
         <Image src="/images/ENRAGED_1.png" alt="Game Logo" layout="responsive" width={1500} height={800} objectFit="cover" />
         <div className="absolute md:space-x-20 transform -translate-x-1/2 -translate-y-1/2 top-[80%] left-1/2">
-          <a href="https://discord.gg/dewJBrpAgH" target='blank' className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Join Discord</a>
-          <a href="https://twitter.com/game_enraged" target='blank' className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Join Twitter</a>
+          <a href="https://discord.gg/dewJBrpAgH" target='blank' className="px-4 py-2 font-bold text-white rounded-md bg-red-600/80 hover:bg-red-700">Join Discord</a>
+          <a href="https://twitter.com/game_enraged" target='blank' className="px-4 py-2 font-bold text-white rounded-md bg-red-600/80 hover:bg-red-700">Join Twitter</a>
         </div>
       </div>
 
@@ -22,7 +22,7 @@ export default function Home() {
           <div className="order-1 w-full p-4 bg-red-600 border border-gray-100 rounded-md md:w-1/2 lg:px-12 md:order-2 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 ">
             <h2 className='pb-2 text-2xl'>Repoussez les limites</h2>
             <p>Plongez au cœur de l'action avec ENRAGED, le dernier cri en matière de FPS. Préparez-vous à une expérience palpitante où chaque décision peut faire la différence entre la victoire et la défaite.</p>
-            <button className="px-4 py-2 mt-4 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">Commencer à jouer</button>
+            <button className="px-4 py-2 mt-4 font-bold text-white rounded bg-red-600/80 hover:bg-red-700">Commencer à jouer</button>
           </div>
         </div>
         
@@ -46,7 +46,7 @@ export default function Home() {
           </ul>
         </div>
 
-        <div className="p-4 my-8 bg-red-400 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">
+        <div className="p-4 mt-8 bg-red-400 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">
           <h2 className="mb-4 text-2xl text-center">Crédits</h2>
           <p className="p-2 text-center bg-red-600 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 ">Une liste de tous ceux qui ont contribué au jeu.</p>
         </div>
