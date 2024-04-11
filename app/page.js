24d94df506a8c3d2a6e67bae841021526ce70d33
LoLoSenPai <loic.dlugosz@gmail.com>
2024-04-11 18:34:11 +0200

    Refactor Home component in app/page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,8 @@
-import Head from 'next/head';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className=''>
-      <Head>
-        <title>TPS Game Landing Page</title>
-        <meta name="description" content="Join our TPS game community" />
-      </Head>
+    <div className="relative" style={{ backgroundImage: 'url(/images/banner.jpg)', backgroundSize: 'cover', backgroundPosition: 'center' }}>
 
       <div className="relative text-center text-white">
         <Image src="/images/ENRAGED_1.png" alt="Game Logo" layout="responsive" width={1500} height={800} objectFit="cover" />
@@ -18,36 +13,37 @@ export default function Home() {
       </div>
 
       <div className="max-w-4xl px-4 mx-auto sm:px-6 lg:px-8">
-        <div className="flex flex-wrap items-center justify-around my-8">
-          <div className="w-full p-4 md:w-1/2 lg:px-12">
+        
+        <div className="flex flex-wrap items-center justify-around p-4 my-8 bg-red-400 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">
+          <div className="w-full p-4 bg-red-600 border border-gray-100 rounded-md md:w-1/2 lg:px-12 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 ">
             <p>Voici du texte à propos du jeu. Expliquez ce qui rend votre jeu unique et pourquoi les gens devraient s'y intéresser.</p>
           </div>
-          <div className="w-full p-4 md:w-1/2">
-            <Image src="/images/banner.jpg" alt="Game Image" width={500} height={300} />
+          <div className="w-1/3 p-4 md:w-[30%]">
+            <Image src="/images/preview-1.png" alt="Game Image" width={829} height={1451} />
           </div>
         </div>
 
-        <div className="flex flex-wrap items-center justify-around my-8">
+        <div className="flex flex-wrap items-center justify-around p-4 my-8 bg-red-400 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">
           <div className="order-2 w-full p-4 md:w-1/2 md:order-1">
-            <Image src="/images/banner.jpg" alt="Another Game Image" width={500} height={300} />
+            <Image src="/images/banner.jpg" alt="Another Game Image" width={500} height={300} className='rounded-lg' />
           </div>
-          <div className="order-1 w-full p-4 md:w-1/2 lg:px-12 md:order-2">
+          <div className="order-1 w-full p-4 bg-red-600 border border-gray-100 rounded-md md:w-1/2 lg:px-12 md:order-2 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 ">
             <p>Plus d'informations sur le jeu, peut-être sur le gameplay ou les caractéristiques spéciales.</p>
           </div>
         </div>
 
-        <div className="my-8">
+        <div className="p-4 my-8 bg-red-400 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">
           <h2 className="mb-4 text-2xl text-center">Roadmap</h2>
-          <ul className="list-disc list-inside">
+          <ul className="p-2 list-disc list-inside bg-red-600 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10">
             <li>Étape 1: Développement du jeu</li>
             <li>Étape 2: Beta Test</li>
             <li>Étape 3: Lancement officiel</li>
           </ul>
         </div>
 
-        <div className="my-8">
+        <div className="p-4 my-8 bg-red-400 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">
           <h2 className="mb-4 text-2xl text-center">Crédits</h2>
-          <p className="text-center">Une liste de tous ceux qui ont contribué au jeu.</p>
+          <p className="p-2 text-center bg-red-600 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 ">Une liste de tous ceux qui ont contribué au jeu.</p>
         </div>
       </div>
     </div>
