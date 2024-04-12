2736bd3f627695857a7fd1e4751054592a7cd270
LoLoSenPai <loic.dlugosz@gmail.com>
2024-04-12 16:34:08 +0200

    Update game logo and social media links in Home component

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
     <div className='pb-6 bg-red-900/10'>
 
       <div className="relative text-center text-white">
-        <Image src="/images/ENRAGED_1.png" alt="Game Logo" layout="responsive" width={1500} height={800} objectFit="cover" />
-        <div className="absolute md:space-x-20 transform -translate-x-1/2 -translate-y-1/2 top-[80%] left-1/2">
+        <Image src="/images/ENRAGED_HOME.png" alt="Game Logo" layout="responsive" width={1500} height={800} objectFit="cover" />
+        <div className="absolute md:space-x-40 transform -translate-x-1/2 -translate-y-1/2 top-[80%] left-1/2">
           <a href="https://discord.gg/dewJBrpAgH" target='blank' className="px-4 py-2 font-bold text-white rounded-md bg-red-600/80 hover:bg-red-700">Join Discord</a>
           <a href="https://twitter.com/game_enraged" target='blank' className="px-4 py-2 font-bold text-white rounded-md bg-red-600/80 hover:bg-red-700">Join Twitter</a>
         </div>
@@ -19,7 +19,7 @@ export default function Home() {
             <Image src="/images/banner.jpg" alt="Another Game Image" width={500} height={300} className='rounded-lg' />
           </div>
           <div className="order-1 w-full p-4 border border-gray-100 rounded-md bg:-red-600 md:w-1/2 lg:px-12 md:order-2 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10">
-            <h2 className='pb-2 text-2xl'>Repoussez les limites</h2>
+            <h2 className='pb-4 text-2xl md:pb-8'>Repoussez les limites</h2>
             <p>Plongez au cœur de l'action avec ENRAGED, le dernier cri en matière de FPS. Préparez-vous à une expérience palpitante où chaque décision peut faire la différence entre la victoire et la défaite.</p>
             <button className="px-4 py-2 mt-4 font-bold text-white rounded bg-red-600/80 hover:bg-red-700">Commencer à jouer</button>
           </div>
@@ -28,7 +28,7 @@ export default function Home() {
         <div className="flex flex-wrap items-center justify-around p-4 my-8 bg-red-400 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">
 
           <div className="w-full p-4 bg-red-600 border border-gray-100 rounded-md md:w-1/2 lg:px-12 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 ">
-            <h2 className='pb-2 text-2xl'>Vos agents</h2>
+            <h2 className='pb-4 text-2xl md:pb-8'>Vos agents</h2>
             <p>Outre vos armes, le choix d'un agent est crucial, car leurs compétences polyvalentes et dangereuses vous accompagnent dans toutes les situations. Chaque agent possède son propre ensemble de compétences, ce qui garantit que chacun de vos moments forts sera unique et mémorable.</p>
           </div>
           <div className="w-1/3 p-4 md:w-[30%]">
@@ -37,7 +37,7 @@ export default function Home() {
         </div>
 
         <div className="p-4 my-8 bg-red-400 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">
-          <h2 className="mb-4 text-2xl text-center">Roadmap</h2>
+          <h2 className="mb-8 text-2xl text-center">Roadmap</h2>
           <ul className="p-2 list-disc list-inside bg-red-600 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10">
             <li>Étape 1: Développement du jeu</li>
             <li>Étape 2: Beta Test</li>
@@ -46,7 +46,7 @@ export default function Home() {
         </div>
 
         <div className="p-4 mt-8 bg-red-400 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 ">
-          <h2 className="mb-4 text-2xl text-center">Crédits</h2>
+          <h2 className="mb-8 text-2xl text-center">Crédits</h2>
           <p className="p-2 text-center bg-red-600 border border-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 ">Une liste de tous ceux qui ont contribué au jeu.</p>
         </div>
       </div>
